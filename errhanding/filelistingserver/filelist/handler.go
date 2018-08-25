2cbb7bea6efb085015c0041698b60c7408eedfb7
肖泽锋 <qq523107430@163.com>
2018-08-26 00:11:33 +0800

    自定义异常处理

--- errhanding/filelistingserver/filelist/handler.go
+++ errhanding/filelistingserver/filelist/handler.go
@@ -4,10 +4,29 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"strings"
 )
 
+const prefix = "/list/"
+
+type userError string
+
+func (e userError) Error() string {
+	return e.Message()
+}
+
+func (e userError) Message() string {
+	return string(e)
+}
+
 func HandlerFileList(writer http.ResponseWriter, request *http.Request) error {
-	path := request.URL.Path[len("/list/"):]
+
+	urlPath := request.URL.Path
+
+	if index := strings.Index(urlPath, prefix); index != 0 {
+		return userError("path must start with " + prefix)
+	}
+	path := urlPath[len("/list/"):]
 	file, err := os.Open(path)
 	if err != nil {
 		return err
--- errhanding/filelistingserver/web.go
+++ errhanding/filelistingserver/web.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"imooc.com/learn-go/errhanding/filelistingserver/filelist"
+	"log"
 	"net/http"
 	"os"
-	"log"
 )
 
 type appHandler func(writer http.ResponseWriter, request *http.Request) error
@@ -12,9 +12,24 @@ type appHandler func(writer http.ResponseWriter, request *http.Request) error
 func errorWrapper(handler appHandler) func(http.ResponseWriter, *http.Request) {
 
 	return func(writer http.ResponseWriter, request *http.Request) {
+		defer func() {
+			if r := recover(); r != nil {
+				log.Printf("Panic: %v", r)
+				http.Error(writer, http.StatusText(http.StatusInternalServerError),
+					http.StatusInternalServerError)
+			}
+		}()
+
 		err := handler(writer, request)
 		if err != nil {
-			log.Printf("Error handing request: %s", err.Error() )
+			log.Printf("Error handing request: %s", err.Error())
+			if userErr, ok := err.(userError); ok {
+				http.Error(writer,
+					userErr.Message(),
+					http.StatusBadRequest)
+				return
+			}
+
 			code := http.StatusOK
 			switch {
 			case os.IsNotExist(err):
@@ -31,9 +46,14 @@ func errorWrapper(handler appHandler) func(http.ResponseWriter, *http.Request) {
 	}
 }
 
-func main() {
-	http.HandleFunc("/list/", errorWrapper(filelist.HandlerFileList))
+// 用户自定义错误
+type userError interface {
+	error
+	Message() string
+}
 
+func main() {
+	http.HandleFunc("/", errorWrapper(filelist.HandlerFileList))
 	err := http.ListenAndServe(":8888", nil)
 	if err != nil {
 		panic(err)
