acba70fe311920e2d8627f4b2c000341521bb4f7
肖泽锋 <qq523107430@163.com>
2018-08-29 09:03:24 +0800

    单线程爬虫

---
+++ crawler/engine/engine.go
@@ -0,0 +1,32 @@
+package engine
+
+import (
+	"fmt"
+	"imooc.com/learn-go/crawler/fetcher"
+	"log"
+)
+
+func Run(seeds ...Request) {
+	var requests []Request
+	for _, r := range seeds {
+		requests = append(requests, r)
+	}
+
+	for len(requests) > 0 {
+		r := requests[0]
+		requests = requests[1:]
+		log.Printf("Fetching: %s", r.Url)
+		body, err := fetcher.Fetch(r.Url)
+		if err != nil {
+			log.Printf("Fetcher: error, fetching url %s, %v", r.Url, err)
+			continue
+		}
+		parseResult := r.ParserFunc(body)
+		// 将slice中的内容加入requests
+		requests = append(requests, parseResult.Requests...)
+		for _, item := range parseResult.Items {
+			fmt.Printf("Got itme: %v \n", item)
+		}
+
+	}
+}
--- crawler/main.go
+++ crawler/main.go
@@ -1,51 +1,14 @@
 package main
 
 import (
-	"net/http"
-	"io/ioutil"
-	"fmt"
-	"golang.org/x/text/transform"
-	"io"
-	"golang.org/x/text/encoding"
-	"bufio"
-	"golang.org/x/net/html/charset"
-	"regexp"
+	"imooc.com/learn-go/crawler/engine"
+	"imooc.com/learn-go/crawler/zhenai/parser"
 )
 
 func main() {
-
-	resp, err := http.Get("http://www.zhenai.com/zhenghun")
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
-	e := determineEncoding(resp.Body)
-	utf8Reader := transform.NewReader(resp.Body, e.NewDecoder())
-	if resp.StatusCode == http.StatusOK {
-		all, err := ioutil.ReadAll(utf8Reader)
-		if err != nil {
-			panic(err)
-		}
-		fmt.Printf("%s\n", all)
-		printCityList(all)
-	}
-}
-func printCityList(contents []byte) {
-	re := regexp.MustCompile(`<a href="http://www.zhenai.com/zhenghun/[0-9a-z]+"[^>]*>[^<]+</a>`)
-	matches := re.FindAll(contents, -1)
-	for _, m :=range matches{
-		fmt.Printf("%s\n", m)
-	}
-	fmt.Printf("Matches found:%d", len(matches))
-}
-
-// 自动判断编码
-func determineEncoding(r io.Reader) encoding.Encoding  {
-	bytes, err := bufio.NewReader(r).Peek(1024)
-	if err != nil {
-		panic(err)
-	}
-	e, _, _ := charset.DetermineEncoding(bytes, "")
-	return e
+	engine.Run(engine.Request{
+		Url:        "http://www.zhenai.com/zhenghun",
+		ParserFunc: parser.ParseCityList,
+	})
 
 }
